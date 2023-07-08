67d439d6ab21af3f85664a50e2ea488b4227f106
hruden <119438895+hruden@users.noreply.github.com>
2023-07-08 17:19:03 +0300

    add team

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,6 +3,7 @@ const openModal = document.getElementById('our-team-btn')
 const ourTeamModal = document.getElementById('our-team')
 const closeModal = document.querySelector('.teem-icon')
 const bodyEl = document.querySelector('body')
+const modalEl = document.querySelector('.footer-modal')
 
 openModal.addEventListener('click', openBtn)
 closeModal.addEventListener('click', openBtn)
@@ -10,70 +11,8 @@ closeModal.addEventListener('click', openBtn)
 
 function openBtn(){
     console.log('hello')
+    // modalEl.showModal()
     ourTeamModal.classList.toggle("footer-modal-hiden")
-    bodyEl.classList.toggle('no-scroll')
+    // bodyEl.classList.toggle('no-scroll')
 
 }
-
-
-// const refs = {
-//     openMenuBtn: document.querySelector("[data-menu-open]"),
-//     closeMenuBtn: document.querySelector("[data-menu-close]"),
-//     menu: document.querySelector("[data-menu]"),
-//     body: document.querySelector("body"),
-//   };
-
-//   refs.openMenuBtn.addEventListener("click", toggleMenu);
-//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
-
-//   function toggleMenu() {
-//     refs.menu.classList.toggle("is-hidden");
-//     refs.body.classList.toggle("no-scroll");
-//   }
-
-/*
-<svg class="teem-icon" width="24" height="24">
-    <use href="/src/images/icons.svg#icon-x-close"></use>
-</svg>
-<ul>
-<li>
-    <img src="" alt=""></img>
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-<li>
-    <img src="" alt="">
-    <p></p>
-    <a href=""></a><a href=""></a></li>
-</ul> */
