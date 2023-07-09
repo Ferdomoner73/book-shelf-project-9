454d9dbc39faaafd6b284f6d734b7a7847a8e8d6
hruden <119438895+hruden@users.noreply.github.com>
2023-07-09 20:08:17 +0300

    fix modal close

--- src/js/footer.js
+++ src/js/footer.js
@@ -18,12 +18,6 @@ window.addEventListener('keydown', (e)=> {
         closeModal()
     }
 })
-
-footerModalEl.addEventListener('click', e => {
-    if (e._isClickWithInModal) return;
-    e.currentTarget.classList.remove('footer-open')
-})
-
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-//   }
+footerModalEl.addEventListener('click', (e) => {
+    if(e.currentTarget === e.target)
+    closeModal()})
