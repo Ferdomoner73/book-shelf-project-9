78660f50606f47fb0f1dd01c612b2ef151860702
hruden <119438895+hruden@users.noreply.github.com>
2023-07-10 14:32:48 +0300

    fix lisner

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,23 +1,32 @@
 const footerModalEl = document.getElementById('our-team')
 const openBtnEl = document.getElementById('our-team-btn')
 const closeBtnEl = document.querySelector('.teem-icon-close')
+const bodyEl = document.querySelector("body")
 
 openBtnEl.addEventListener('click', openModal)
 closeBtnEl.addEventListener('click', closeModal)
 
 function openModal(){
     footerModalEl.classList.add('footer-open')
+    bodyEl.classList.add('no-scroll')
+    window.addEventListener('keydown', downEsc)
 }
 
 function closeModal(){
     footerModalEl.classList.remove('footer-open')
+    bodyEl.classList.remove('no-scroll')
+    window.removeEventListener('keydown', downEsc)
+
 }
 
-window.addEventListener('keydown', (e)=> {
-    if (e.key === 'Escape') {
-        closeModal()
-    }
-})
+
 footerModalEl.addEventListener('click', (e) => {
     if(e.currentTarget === e.target)
     closeModal()})
+
+function downEsc(e){
+    if (e.key === 'Escape') 
+    console.log('hi')
+    closeModal()
+
+}
