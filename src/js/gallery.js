97379c9467e4fde7764277aeaae1248dd5f91db7
Efim1298 <efimchuk12@gmail.com>
2023-07-11 20:11:57 +0300

    add scroll-down files, update gallery.js(loaderHidden())

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,11 +1,10 @@
 import _ from 'lodash';
 import { loaderShow, loaderHidden } from './loader';
 
-
 // // CODE FOR RENDERING MAINPAGE
 
-const galleryRef = document.querySelector(".five-books-cards-wrapper");
-const galleryListRef = document.querySelector(".gallery-list");
+const galleryRef = document.querySelector('.five-books-cards-wrapper');
+const galleryListRef = document.querySelector('.gallery-list');
 
 function createMainPageCard(book) {
   return `<li class="gallery-list-item">
@@ -31,7 +30,7 @@ function createMainPageCard(book) {
 }
 async function fetchingTopBooks() {
   try {
-    const url = "https://books-backend.p.goit.global/books/top-books";
+    const url = 'https://books-backend.p.goit.global/books/top-books';
     const response = await fetch(url);
     const data = await response.json();
     return data;
@@ -40,46 +39,40 @@ async function fetchingTopBooks() {
   }
 }
 
-
-fetchingTopBooks().then((response) => renderingHomePage(response));
+fetchingTopBooks().then(response => renderingHomePage(response));
 function renderingHomePage() {
-  galleryRef.innerHTML = "";
-    fetchingTopBooks().then((response) => {
+  galleryRef.innerHTML = '';
+  fetchingTopBooks().then(response => {
+    galleryRef.insertAdjacentHTML(
+      'beforeend',
+      '<h2 class="gallery-title">Best Sellers <span class="blue-title-color">Books</span></h2>'
+    );
+    for (let elem of response) {
       galleryRef.insertAdjacentHTML(
-        "beforeend",
-        '<h2 class="gallery-title">Best Sellers <span class="blue-title-color">Books</span></h2>'
-      );
-      for (let elem of response) {
-        galleryRef.insertAdjacentHTML(
-          "beforeend",
-          `<h3 class="gallery-category">${elem.books[0].list_name}</h3>
+        'beforeend',
+        `<h3 class="gallery-category">${elem.books[0].list_name}</h3>
             <ul class="gallery-list"></ul>`
-        );
-        let mainPageCategories = document.querySelectorAll(".gallery-list");
-        const galleryListRef =
-          mainPageCategories[mainPageCategories.length - 1];
-        for (let i = 0; i < 5; i++) {
-          galleryListRef.insertAdjacentHTML(
-            "beforeend",
-            createMainPageCard(elem.books[i])
-          );
-        }
-        galleryRef.insertAdjacentHTML(
-          "beforeend",
-          `<button class="see-more-button" data-category="${elem.books[0].list_name}">See more</button>`
+      );
+      let mainPageCategories = document.querySelectorAll('.gallery-list');
+      const galleryListRef = mainPageCategories[mainPageCategories.length - 1];
+      for (let i = 0; i < 5; i++) {
+        galleryListRef.insertAdjacentHTML(
+          'beforeend',
+          createMainPageCard(elem.books[i])
         );
       }
-    });
-  }
-
-
-
-
-
+      galleryRef.insertAdjacentHTML(
+        'beforeend',
+        `<button class="see-more-button" data-category="${elem.books[0].list_name}">See more</button>`
+      );
+    }
+    loaderHidden();
+  });
+}
 
 // Render Cards by Category CODE
 
- async function fetchByCategory(category) {
+async function fetchByCategory(category) {
   try {
     const url = `https://books-backend.p.goit.global/books/category?category=${category}`;
     const response = await fetch(url);
@@ -90,10 +83,9 @@ function renderingHomePage() {
   }
 }
 
-
- function createMoreBooks(booksArr) {
+function createMoreBooks(booksArr) {
   const bookCard = booksArr
-    .map((book) => {
+    .map(book => {
       const markup = `<li class="gallery-list-item">
             <div class="gallery-list-item-wrapper" data-book-id="${book._id}">
                <div class="overlay-card-wrapper">
@@ -117,7 +109,7 @@ function renderingHomePage() {
 
       return markup;
     })
-    .join("");
+    .join('');
 
   return bookCard;
 }
