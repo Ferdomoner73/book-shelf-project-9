40575505f0d4cc4e449198fd22de492eeb019784
YuriyOhii <uaogiy@gmail.com>
2023-07-11 13:56:14 +0300

    adds functions rendering shopping list

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,73 +1,79 @@
-// // імпортуємо розмітку для картки та клас для роботи із сховищем
-// import cardTemplate from '../templates/sl-book-card.js';
-// import LocalStorage from './Classes/local-storage.js';
-// const local = new LocalStorage();
+// імпортуємо розмітку для картки та клас для роботи із сховищем
+import cardTemplate from '../templates/sl-book-card.js';
+import LocalStorage from './Classes/local-storage.js';
+const local = new LocalStorage();
+
+// const shopListBtnRef = document.querySelector('.js-shopping-list-btn');
+const categoriesListSecRef = document.querySelector('.categories-list-section');
+const galleryRef = document.querySelector('.five-books-cards-wrapper');
+
+// shopListBtnRef.addEventListener('click', onShoppingListBtnClick);
+
+// працює при переході на shopping list
+function onShoppingListBtnClick() {
+  local.init();
+  chooseWhatToRender();
+}
+
+// відозмінює сторінку перед самим рендером
+function prepareToRender() {
+  console.log('We preparing now...');
+  categoriesListSecRef.classList.add('visually-hidden');
+  galleryRef.innerHTML =
+    '<h2 class="gallery-title">Shopping <span class="blue-title-color">List</span></h2>';
+}
+
+// перевіряє чи є що відображати з сховища
+function chooseWhatToRender() {
+  const savedList = local.getBooks();
+  if (!savedList.length) {
+    renderNothingToShow();
+    return;
+  }
+  renderBooksCards(savedList);
+}
 
 
-// // функція, що приймає масив об'єктів і рендерить картки
-// function renderShopListBookCards(data) {
-//   console.log("it is render function")
-//   const markup = data.map(cardTemplate).join('');
-//   document.body.insertAdjacentHTML('afterbegin', markup)
-// }
 
-// const containerRef = document.querySelector('.sl-card-list');
+// якщо нема що відобразити
+function renderNothingToShow() {
+  console.log('There is nothing to show');
+    prepareToRender();
+    const markup =
+        '<div class="sl-wrap"><p class="sl-text">This page is empty, add some books and proceed to order.</p > <div class="sl-img-container"><img src="/src/images/shopping-list/books-tower.png" alt="Books tower" width="322" height="241"></div></div > ';
+  galleryRef.insertAdjacentHTML('beforeend', markup);
+}
+
+// функція, що приймає масив об'єктів і рендерить картки
+function renderShopListBookCards(data) {
+  console.log('it is render function');
+  console.log(data);
+  prepareToRender();
+  const markup = data.map(cardTemplate).join('');
+  galleryRef.insertAdjacentHTML('beforeend', markup);
+}
+
+// відмальовує збережені в сховищі об'єкти та додає слухач на видалення
+function renderBooksCards(data) {
+  console.log('Let me show what we have?');
+  renderShopListBookCards(data);
+  galleryRef.addEventListener('click', onDeleteBtnClick);
+}
+
+// видаляє картку з сховища по кліку на кнопці видалення
+function onDeleteBtnClick({ target }) {
+  console.log(target);
+  if (!target.classList.contains('sl-card-icon')) return;
+  local.deleteBookById(target.parentElement.parentElement.id);
+  chooseWhatToRender();
+}
+
 // // тимчасові елементи, що замінюють хедер і поп ап
-// const btnAddRef = document.querySelector('.js-shopping-add-book');
-// const btnRef = document.querySelector('.js-shopping-list-btn');
-// let counter = 0;
-
-// btnRef.addEventListener('click', onShoppingListBtnClick);
+// const btnAddRef = document.querySelector('.js-add-book-btn');
 // btnAddRef.addEventListener('click', addBookToStorage);
 
-// // працює при переході на shopping list
-// function onShoppingListBtnClick() {
-//   prepareToRender();
-//   local.init();
-//   chooseWhatToRender();
-// }
-
-// // перевіряє чи є що відображати з сховища
-// function chooseWhatToRender() {
-//   const savedList = local.getBooks();
-//   if (!savedList.length) {
-//     renderNothingToShow();
-//     return;
-//   }
-//   renderBooksCards(savedList);
-// }
-
-// // якщо нема що відобразити
-// function renderNothingToShow() {
-//   // вставити необхідне 
-//   containerRef.innerHTML = '';
-//   console.log('There is nothing to show');
-// }
-
-// // відмальовує збережені в сховищі об'єкти та додає слухач на видалення
-// function renderBooksCards(data) {
-//   console.log('Let me show what we have?');
-//   renderShopListBookCards(data);
-//   // вставити необхідне
-//   containerRef.addEventListener('click', onDeleteBtnClick);
-// }
-
-// // видаляє картку з сховища по кліку на кнопці видалення
-// function onDeleteBtnClick({ target }) {
-//   // вставити необхідне
-//   if (!target.classList.contains('sl-card-delete-btn')) return;
-//   // вставити необхідне
-//   local.deleteBookById(target.parentElement.id);
-//   chooseWhatToRender();
-// }
-
-// // відозмінює сторінку перед самим рендером
-// function prepareToRender() {
-//   console.log('We preparing now...');
-// }
-
-
-// //тимчасова функція для отримання даних та їх завантаження у сховище
+// // тимчасова функція для отримання даних та їх завантаження у сховище
+// let counter = 0;
 // function addBookToStorage() {
 //   fetch('https://books-backend.p.goit.global/books/top-books')
 //     .then(r => r.json())
