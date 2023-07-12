e2f9445a2fcf7d903bd036a30f2d239992e66331
Efim1298 <efimchuk12@gmail.com>
2023-07-12 13:12:10 +0300

    update scroll

--- src/js/scroll-down.js
+++ src/js/scroll-down.js
@@ -1,7 +1,7 @@
 const toDownBtn = document.querySelector('.btn-to-down');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 500) {
+  if (window.pageYOffset > 1050) {
     toDownBtn.classList.add('btn-to-down--visible');
   } else {
     toDownBtn.classList.remove('btn-to-down--visible');
