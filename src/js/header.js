6fc3e6e1f3c94dbd64fdc7ca5c710d0a263b69c4
NazarkoKril <122899742+NazarkoKril@users.noreply.github.com>
2023-07-13 04:58:32 +0300

    update burger

---
+++ src/js/header.js
@@ -0,0 +1,28 @@
+// modal burger
+const backdropMobile = document.querySelector('.js-backdrop-mobile');
+const openButtonMobile = document.querySelector('.js-open-menu');
+const svgOpenEl = openButtonMobile.firstElementChild;
+const closeButtonMobile = document.querySelector('.js-close-menu');
+
+const btnHeaderEls = document.querySelector('.header__togglers')
+btnHeaderEls.addEventListener('click', (e) => {
+    
+    if (e.target.nodeName !== "svg" && e.target.nodeName !== "use") {
+        return
+    }
+
+    if (svgOpenEl.classList.contains('icon-burger')) {
+        backdropMobile.classList.add('is-open');
+        closeButtonMobile.classList.remove('visually-hidden');
+        openButtonMobile.classList.add('visually-hidden');
+        svgOpenEl.classList.remove('icon-burger');
+        return
+    }
+
+    backdropMobile.classList.remove('is-open');
+    closeButtonMobile.classList.add('visually-hidden');
+    openButtonMobile.classList.remove('visually-hidden');
+    svgOpenEl.classList.add('icon-burger');
+} )
+
+// logic mobile
